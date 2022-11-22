19253a8199c52c10b1fe44fd0b2dee4ddb057e6a
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-22 20:10:24 +0800

    Add additional examples

---
+++ component/some_component.ts
@@ -0,0 +1,4 @@
+import users from "../data/users.json"
+
+const user = users[0];
+
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello world")
+import _users from "./data/users.json"
+import { User } from "./types/User";
+
+const users = _users as User[];
+const user = users[0];
+
+console.log(user.first_name)
+
+
